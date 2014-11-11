2ea44ad114ae0c7bbaf615b925a81525f74c8af5
Dunderfelt, Daniel <daniel.dunderfelt@activearkjwt.com>
2014-11-11 20:08:25 +0200

    Updated middleware ref

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -12,7 +12,7 @@ class RouteServiceProvider extends ServiceProvider {
 	 */
 	protected $middleware = [
 		'auth' => 'Dunderfelt\Tony\Http\Middleware\Authenticated',
-		'auth.basic' => 'Dunderfelt\Tony\Http\Middleware\AuthenticatedWithBasicAuth',
+		'auth.basic' => 'Illuminate\Auth\Middleware\AuthenticatedWithBasicAuth',
 		'guest' => 'Dunderfelt\Tony\Http\Middleware\IsGuest'
 	];
 
